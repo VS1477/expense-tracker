628e067d25fa88e022137b9d0cc2fcf0a0e5e01a
Vishal Singh <119868252+VS1477@users.noreply.github.com>
2026-02-01 23:40:01 +0530

    more changes to spinner

--- frontend/src/componenets/spinner.jsx
+++ frontend/src/componenets/spinner.jsx
@@ -2,13 +2,11 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <>
-      <div className="d-flex justify-content-center">
-        <div className="spinner-border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
+    <div className="app-spinner-wrapper" aria-hidden="false" aria-label="Loading">
+      <div className="app-spinner" role="status">
+        <span className="sr-only">Loading...</span>
       </div>
-    </>
+    </div>
   );
 };
 
